4f6d9fa97bdfa0abbfeea814d992b8b6e4afffa7
SoniEx2 <endermoneymod@gmail.com>
2015-06-15 18:01:44 -0300

    Tweaked splashes

--- src/main/java/com/github/soniex2/splash/Splash.java
+++ src/main/java/com/github/soniex2/splash/Splash.java
@@ -39,68 +39,34 @@ public class Splash {
     }
 
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        ArrayList<ModSplashes> splashes = new ArrayList<ModSplashes>();
-        for (ModContainer mod : Loader.instance().getModList()) {
-            String logname = mod.getModId();
-            String filename = "/assets/splash/splashes/" + mod.getModId() + ".txt";
-            InputStream is = getClass().getResourceAsStream(filename);
+    public void preInit(final FMLPreInitializationEvent event) {
+
+        final ArrayList<ModSplashes> splashes = new ArrayList<ModSplashes>();
+
+        for (final ModContainer mod : Loader.instance().getModList()) {
+            final String filename = "/assets/splash/splashes/" + mod.getModId() + ".txt";
+            // TODO let other mods add their own splashes
+            final InputStream is = getClass().getResourceAsStream(filename);
             if (is == null) continue;
-            ArrayList<String> modSplashes = new ArrayList<String>();
+
+            final ArrayList<String> modSplashes = new ArrayList<String>();
+            String logname = mod.getModId();
             BufferedReader r = null;
+
             try {
                 r = new BufferedReader(new InputStreamReader(is, Charsets.UTF_8));
                 String s;
-                /*
-                START Logname Flag (#!len:name on first line).
-                Used by BuildCraft and a few other mods.
-                TODO: rewrite it to be more intuitive
-                 */
+
                 s = r.readLine();
-                boolean flag = false;
-                if (s != null && s.trim()/* NOTE: this doesn't change the value of "s" */.startsWith("#!")) {
-                    if (s.indexOf('!') + 1 <= s.length()) {
-                        String[] split = s.substring(s.indexOf('!') + 1).split(":", 2);
-                        if (split.length >= 2) {
-                            try {
-                                int i = Integer.parseInt(split[0]);
-                                if (i > 0) {
-                                    StringBuilder sb = new StringBuilder(i).append(split[1]);
-                                    while (sb.length() < i) {
-                                        s = r.readLine();
-                                        if (s != null) {
-                                            sb.append("\n").append(s);
-                                        } else {
-                                            break;
-                                        }
-                                    }
-                                    if (sb.length() >= i) {
-                                        flag = true;
-                                        sb.setLength(i);
-                                        logname = sb.toString();
-                                    } else {
-                                        event.getModLog().error("Errors were found while parsing file " + filename + ". " +
-                                                "Could not parse logname. Defaulting to " + logname);
-                                    }
-                                } else {
-                                    event.getModLog().error("Errors were found while parsing file " + filename + ". " +
-                                            "Invalid logname length. Using default logname: " + logname);
-                                }
-                            } catch (NumberFormatException e) {
-                                event.getModLog().error("Errors were found while parsing file " + filename + ". " +
-                                        "Invalid logname length. Using default logname: " + logname, e);
-                            }
-                        } // else treat it like it's a comment
-                    } // else treat it like it's a comment
-                }
-                if (!flag && s != null) {
+                if (s != null && s.trim()/* NOTE: this doesn't change the value of "s" */.startsWith("#!")
+                        && s.indexOf('!') + 1 <= s.length()) {
+                    logname = processEscapes(s.substring(s.indexOf('!') + 1));
+                } else if (s != null) {
                     s = s.trim();
                     if (!s.startsWith("#") && !s.isEmpty())
                         modSplashes.add(s);
                 }
-                /*
-                END Logname Flag.
-                 */
+
                 while ((s = r.readLine()) != null) {
                     s = s.trim();
                     if (!s.startsWith("#") && !s.isEmpty())
@@ -116,34 +82,46 @@ public class Splash {
                     }
                 }
             }
+
             if (!modSplashes.isEmpty()) {
                 splashes.add(new ModSplashes(mod.getModId(), logname, modSplashes));
             }
         }
+
         if (!splashes.isEmpty()) {
-            ModSplashes splash = splashes.get(rand.nextInt(splashes.size()));
-            String s = splash.splashes.get(rand.nextInt(splash.splashes.size()));
-            Matcher m = p.matcher(s);
-            StringBuilder sb = new StringBuilder();
-            int last = 0;
-            while (m.find()) {
-                sb.append(s.substring(last, m.start()));
-                String s2;
-                switch (m.group(1).charAt(0)) {
-                    case 'n':
-                        s2 = "\n";
-                        break;
-                    default:
-                        s2 = m.group(1);
-                }
-                sb.append(s2);
-                last = m.end();
-            }
-            sb.append(s.substring(last));
-            s = sb.toString();
-            for (String s1 : s.split("\n"))
+
+            final ModSplashes splash = splashes.get(rand.nextInt(splashes.size()));
+            final String s = processEscapes(splash.splashes.get(rand.nextInt(splash.splashes.size())));
+
+            for (final String s1 : s.split("\n"))
                 if (!s1.isEmpty())
                     LogManager.getLogger(splash.logname).info(s1);
+
         }
+    }
+
+    private String processEscapes(final String s) {
+        final Matcher m = p.matcher(s);
+        final StringBuilder sb = new StringBuilder();
+
+        int last = 0;
+        while (m.find()) {
+            sb.append(s.substring(last, m.start()));
+
+            final String s2;
+            switch (m.group(1).charAt(0)) {
+                case 'n':
+                    s2 = "\n";
+                    break;
+                default:
+                    s2 = m.group(1);
+            }
+            sb.append(s2);
+
+            last = m.end();
+        }
+        sb.append(s.substring(last));
+
+        return sb.toString();
     }
 }
