78841ddf5034d124788d2f3788c71cd94d2cc8da
SoniEx2 <endermoneymod@gmail.com>
2015-07-15 12:15:12 -0300

    ???

--- src/main/java/com/github/soniex2/splash/Splash.java
+++ src/main/java/com/github/soniex2/splash/Splash.java
@@ -11,16 +11,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * @author soniex2
  */
-@Mod(modid = "splash", name = "Splash", version = "0.1.1")
+@Mod(modid = "splash", name = "Splash", version = "0.1.2")
 public class Splash {
     private static final Random rand = new Random();
 
@@ -36,6 +34,11 @@ public class Splash {
             this.logname = logname;
             this.splashes = splashes;
         }
+
+        @Override
+        public String toString() {
+            return "{modid=" + modid + ",logname=" + logname + ",splashes=" + splashes + "}";
+        }
     }
 
     @Mod.EventHandler
@@ -43,50 +46,63 @@ public class Splash {
 
         final ArrayList<ModSplashes> splashes = new ArrayList<ModSplashes>();
 
+        final Set<Class> classes = new HashSet<Class>();
+
         for (final ModContainer mod : Loader.instance().getModList()) {
-            final String filename = "/assets/splash/splashes/" + mod.getModId() + ".txt";
-            // TODO let other mods add their own splashes
-            final InputStream is = getClass().getResourceAsStream(filename);
-            if (is == null) continue;
+            classes.add(mod.getClass());
+            if (mod.getMod() != null) {
+                classes.add(mod.getMod().getClass());
+            }
+        }
 
-            final ArrayList<String> modSplashes = new ArrayList<String>();
-            String logname = mod.getModId();
-            BufferedReader r = null;
+        for (final ModContainer mod : Loader.instance().getModList()) {
+            final Set<String> modSplashes = new HashSet<String>();
+            String logname = null;
+            for (final Class clazz : classes) {
+                final String filename = "/assets/splash/splashes/" + mod.getModId() + ".txt";
+                final InputStream is = clazz.getResourceAsStream(filename);
+                if (is == null) continue;
 
-            try {
-                r = new BufferedReader(new InputStreamReader(is, Charsets.UTF_8));
-                String s;
+                if (logname == null)
+                    logname = mod.getModId();
+                BufferedReader r = null;
 
-                s = r.readLine();
-                if (s != null && s.trim()/* NOTE: this doesn't change the value of "s" */.startsWith("#!")
-                        && s.indexOf('!') + 1 <= s.length()) {
-                    logname = processEscapes(s.substring(s.indexOf('!') + 1));
-                } else if (s != null) {
-                    s = s.trim();
-                    if (!s.startsWith("#") && !s.isEmpty())
-                        modSplashes.add(s);
-                }
+                try {
+                    r = new BufferedReader(new InputStreamReader(is, Charsets.UTF_8));
+                    String s;
 
-                while ((s = r.readLine()) != null) {
-                    s = s.trim();
-                    if (!s.startsWith("#") && !s.isEmpty())
-                        modSplashes.add(s);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (r != null) {
-                    try {
-                        r.close();
-                    } catch (IOException ignored) {
+                    s = r.readLine();
+                    if (s != null && s.trim()/* NOTE: this doesn't change the value of "s" */.startsWith("#!")
+                            && s.indexOf('!') + 1 <= s.length()) {
+                        logname = processEscapes(s.substring(s.indexOf('!') + 1));
+                    } else if (s != null) {
+                        s = s.trim();
+                        if (!s.startsWith("#") && !s.isEmpty())
+                            modSplashes.add(s);
+                    }
+
+                    while ((s = r.readLine()) != null) {
+                        s = s.trim();
+                        if (!s.startsWith("#") && !s.isEmpty())
+                            modSplashes.add(s);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (r != null) {
+                        try {
+                            r.close();
+                        } catch (IOException ignored) {
+                        }
                     }
                 }
             }
-
             if (!modSplashes.isEmpty()) {
-                splashes.add(new ModSplashes(mod.getModId(), logname, modSplashes));
+                splashes.add(new ModSplashes(mod.getModId(), logname, new ArrayList<String>(modSplashes)));
             }
         }
+
+        System.out.println(splashes);
 
         if (!splashes.isEmpty()) {
 
